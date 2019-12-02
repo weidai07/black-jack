fb3bfeaf5016628093f8e11c5ec82fe9d5f67f35
Student <example@example.com>
2019-12-02 11:04:18 -0800

    Will add a test to see if score under 21 works properly

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -9,8 +9,14 @@ describe ('Player', ()=>
   });
   test ('Should correctly test if the score of the user is 21', ()=>
   {
-    var player1 = new Player (11,10)
+    var player1 = new Player (11,10);
     player1.addScore();
     expect(player1.checkScore()).toEqual("You have 21 points");
-  })
+  });
+  test ('Should correctly test if the score of the user is under 21', ()=>
+  {
+    var player1 = new Player(10,9);
+    player1.addScore();
+    expect(player1.checkScore()).toEqual("You have less than 21 points");
+  });
 })
