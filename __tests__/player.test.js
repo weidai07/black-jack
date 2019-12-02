81e65a5355b48fd555a1d057f920a33d1015dd90
Student <example@example.com>
2019-12-02 10:58:05 -0800

    Will make the over 21 test work

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -4,6 +4,7 @@ describe ('Player', ()=>
   test ('Should correctly test if the score of the user is over 21', ()=>
   {
     var player1 = new Player (12,12);
+    player1.addScore();
     expect(player1.checkScore()).toEqual("You busted");
   });
 })
