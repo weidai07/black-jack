954d54aa7c1cf79916c7e0d57a070777f70dbd4d
Student <example@example.com>
2019-12-02 13:11:02 -0800

    Test will fail successfully to check if the user gets 21 after a hit

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -3,27 +3,34 @@ describe ('Player', ()=>
 {
   test ('Should correctly test if the score of the user is over 21', ()=>
   {
-    var player1 = new Player (12,12);
+    let player1 = new Player (12,12);
     player1.addScore();
     expect(player1.checkScore()).toEqual("You busted");
   });
   test ('Should correctly test if the score of the user is 21', ()=>
   {
-    var player1 = new Player (11,10);
+    let player1 = new Player (11,10);
     player1.addScore();
     expect(player1.checkScore()).toEqual("You have 21 points");
   });
   test ('Should correctly test if the score of the user is under 21', ()=>
   {
-    var player1 = new Player(10,9);
+    let player1 = new Player(10,9);
     player1.addScore();
     expect(player1.checkScore()).toEqual("You have less than 21 points");
   });
   test ('Should correctly test if a hit makes the user bust', () =>
   {
-    var player1 = new Player(10,4);
+    let player1 = new Player(10,4);
     player1.addScore();
     player1.hitScore(9);
     expect(player1.checkScore()).toEqual("You busted");
-  })
+  });
+  test ('Should correctly test if a hit makes the user score equal 21', () =>
+  {
+    let player1 = new Player(6,5);
+    player1.addScore();
+    player1.hitScore(10);
+    expect(player1.checkScore()).toEqual();
+  });
 });
