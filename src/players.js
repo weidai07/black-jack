90d1ca268399bd13053e0b4690c86e2a8d817d3b
Student <example@example.com>
2019-12-02 10:42:45 -0800

    Will create a basic score tester, and a file that will test these testers

---
+++ src/players.js
@@ -0,0 +1,24 @@
+export class Player
+{
+  constructor (card1, card2)
+  {
+    this.card1 = card1,
+    this.card2 = card2,
+    this.score = 0
+  }
+  addScore()
+  {
+    return this.score = this.card1 +this.card2;
+  }
+  checkScore()
+  {
+    if (this.score<21)
+    {
+      return "You have less than 21 points"
+    }
+    else if (this.score==21)
+      return "You have 21 points"
+    else
+      return "You busted"
+  }
+}
