66a7bc3ca62d8d6cf55a43baa2dcd6de61a78823
Student <example@example.com>
2019-12-04 15:15:54 -0800

    "connect logic to api calls"

--- src/main.js
+++ src/main.js
@@ -9,11 +9,17 @@ import {Cards} from './cards.js';
 
 // let player1;
 let cards = new Cards();
-let player1 = new Player(10,3, false);
+let player1;
 $(document).ready(function() {
     (async () => {
         let newDeck = await cards.makeDecks(1);
         const id = cards.getDeck_id(newDeck);
+        let c1 = await cards.getValue(await cards.nextCard(id));
+        let c2 = await cards.getValue(await cards.nextCard(id));
+        player1 = new Player(c1,c2, false);
+        player1.addScore();
+        console.log(c1);
+        console.log(c2);
         console.log(id);
         console.log(newDeck);
         console.log(player1);
@@ -21,6 +27,9 @@ $(document).ready(function() {
         $("#start").click(function(){    
         (async () => {
             let response = await cards.nextCard(id);
+            player1.hitScore(cards.getValue(response));
+            console.log(player1.checkScore());
+            console.log(player1);
             console.log(cards.getValue(response));
             console.log(response.cards[0].value);
             console.log(newDeck);
