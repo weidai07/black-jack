71773aa384c749817601cac492861ec86eae458e
Jamila Aliyeva and Jake Ash <jake@>
2019-12-04 10:10:58 -0800

    Will make new deck and get randoms cards from deck

--- src/main.js
+++ src/main.js
@@ -7,30 +7,27 @@ import {Cards} from './cards.js';
 
 
 
+// let player1;
+let cards = new Cards();
+let player1 = new Player(10,3, false);
 $(document).ready(function() {
-    let player1;
-    $("#start").click(function(){
-          player1 = new Player(10,3, false);
+    (async () => {
+        let newDeck = await cards.makeDecks(1);
+        const id = cards.getDeck_id(newDeck);
+        console.log(id);
+        console.log(newDeck);
+        console.log(player1);
+
+        $("#start").click(function(){    
         (async () => {
-            let cards = new Cards();
-            let x = await cards.makeDecks(1);
-            const id = cards.getDeck_id(x);
-            let currentCard = await cards.nextCard(id);
-            console.log(currentCard);
-            
-            // $("#hit").click(player1.hitScore(currentCard.cards[0].value)); // current card is var form async
-
-
-    //this is to test all the api calls and methods, because i cant seem to test them with jest
-
-    
-        //Makes a new deck every time we click hit
-
-        console.log(cards.getRemaining(x));
-        console.log(cards.getDeck_id(x));
-        console.log(cards.getValue(currentCard));
-        // currentCard = await cards.nextCard(id);
-        console.log(cards.getValue(currentCard));
-    })();
-    });
+            let response = await cards.nextCard(id);
+            console.log(cards.getValue(response));
+            console.log(response.cards[0].value);
+            console.log(newDeck);
+          })();
+        });
+    })();  
 });
+ 
+       
+         
